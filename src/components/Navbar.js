f95ab2c3d46d44e9266e6d3a99a7200bb28a7f50
Vineet Tiwari <166735997+vineettiwari1708@users.noreply.github.com>
2026-01-01 13:48:22 +0530

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../css/navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/portfolio">Portfolio</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
